96f36afab9197694412191019fa270ec877489a3
周旭 <zhouxusoft@gmail.com>
2023-08-12 21:53:05 +0800

    ..

---
+++ project-display-backend/create_db.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import pymysql
+import os
+
+load_dotenv()
+# 从配置文件中读取数据
+MYSQL_DATABASE_HOST = os.getenv("MYSQL_DATABASE_HOST")
+MYSQL_DATABASE_USER = os.getenv("MYSQL_DATABASE_USER")
+MYSQL_DATABASE_PASSWORD = os.getenv("MYSQL_DATABASE_PASSWORD")
+
+db = pymysql.connect(
+    host=MYSQL_DATABASE_HOST,
+    user=MYSQL_DATABASE_USER,
+    password=MYSQL_DATABASE_PASSWORD,
+    charset="utf8mb4"
+)
+dbcursor = db.cursor()
+
+def create_database():
+    # 创建数据库
+    dbcursor.execute("CREATE DATABASE IF NOT EXISTS `project_display`")
+    # 创建数据表
+    dbcursor.execute("CREATE TABLE IF NOT EXISTS `access_token`(\
+                    `id` int NOT NULL AUTO_INCREMENT,\
+                    `userId` int NOT NULL,\
+                    `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,\
+                    `setTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\
+                    PRIMARY KEY (`id`) USING BTREE\
+                    ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;")
