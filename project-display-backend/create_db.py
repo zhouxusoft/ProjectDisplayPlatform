cae22a5a8340c99a691259852bab331e85146f62
周旭 <zhouxusoft@gmail.com>
2023-08-13 21:52:44 +0800

    ..

--- project-display-backend/create_db.py
+++ project-display-backend/create_db.py
@@ -22,14 +22,20 @@ def create_database():
     dbcursor.execute("CREATE DATABASE IF NOT EXISTS `project_display`")
     # 创建数据表
     dbcursor.execute("CREATE TABLE `user_table`  (\
-                        `user_id` int NOT NULL AUTO_INCREMENT COMMENT '序号',\
-                        `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',\
-                        `nickname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '昵称',\
-                        `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '哈希加密后的密码',\
-                        `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',\
-                        `lastest_login_time` datetime NULL DEFAULT NULL COMMENT '最近一次登陆时间',\
-                        `status` int NOT NULL DEFAULT 0 COMMENT '账户状态（正常1、封禁0、注销2）',\
+                        `user_id` int NOT NULL AUTO_INCREMENT,\
+                        `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,\
+                        `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,\
+                        `nickname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,\
+                        `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\
+                        `lastest_login_time` datetime NULL DEFAULT NULL,\
+                        `status` int NOT NULL DEFAULT 0 COMMENT '（正常1、封禁0、注销2）',\
                         PRIMARY KEY (`user_id`) USING BTREE\
                         ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;")
-    
+    dbcursor.execute("CREATE TABLE IF NOT EXISTS `access_token`(\
+                        `id` int NOT NULL AUTO_INCREMENT,\
+                        `user_id` int NOT NULL,\
+                        `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,\
+                        `set_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\
+                        PRIMARY KEY (`id`) USING BTREE\
+                        ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;")
     db.close()
