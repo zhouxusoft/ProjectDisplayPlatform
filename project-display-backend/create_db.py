7c121a3b6116f7b732e2c234ed5ece1c845ff259
周旭 <zhouxusoft@gmail.com>
2023-09-17 23:14:22 +0800

    ..

--- project-display-backend/create_db.py
+++ project-display-backend/create_db.py
@@ -57,6 +57,12 @@ def create_database():
                         `tag_hot` int NULL DEFAULT NULL,\
                         PRIMARY KEY (`id`) USING BTREE\
                         ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;")
-
+    dbcursor.execute("CREATE TABLE IF NOT EXISTS `languages` (\
+                        `id` int NOT NULL,\
+                        `language_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,\
+                        `language_color` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,\
+                        `language_hot` int NULL DEFAULT NULL,\
+                        PRIMARY KEY (`id`) USING BTREE\
+                        ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;")
 
     db.close()
