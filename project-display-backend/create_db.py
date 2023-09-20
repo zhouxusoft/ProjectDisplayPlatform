ee3d4c0a9e23ee5f5923dcb20031c7936e14ac00
周旭 <zhouxusoft@gmail.com>
2023-09-20 15:21:58 +0800

    数据库字段更新

--- project-display-backend/create_db.py
+++ project-display-backend/create_db.py
@@ -53,6 +53,7 @@ def create_database():
                         `main_language_id` int NULL DEFAULT NULL,\
                         `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\
                         `starred_num` int NULL DEFAULT NULL,\
+                        `upline_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,\
                         `update_time` datetime NULL DEFAULT NULL,\
                         PRIMARY KEY (`id`) USING BTREE\
                         ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;")
